5b5a46a6a9d84ad55633b33b222e18a26500ab45
erenerdo <ererdogan20@gmail.com>
2018-07-04 08:51:05 -0400

    updated to get 24 permutations

--- fantasy_order.py
+++ fantasy_order.py
@@ -1,17 +1,19 @@
 import random
+import itertools
 
-wc_teams = ['France', 'Uruguay', 'Spain/Russia', 'Croatia/Denmark', 'Brazil/Mexico', 'Belgium/Japan', 'Sweden/Switzerland', 'Colombia/England']
+wc_teams = ['France/Uruguay', 'Russia/Croatia', 'Brazil/Belgium', 'Sweden/England']
 
 fantasy_people = ['Andrew Weiser', 'Ben Vitello', 'CJ Aydin', 'Chris Tran', 'Eren Erdogan', 'Eric Revich', 'Mike Themis', 'Pat Nizza', 'Scott Kessler', 'Sean Slavin', 'Sunny Polepalli', 'Tim Sawma']
 
+permutations = list(itertools.permutations(wc_teams))
 
-def print_order(wc_team, fantasy_people):
-    print('{}: '.format(wc_team))
+def print_order(order, fantasy_people):
+    print('1st {}, 2nd {}, 3rd {}, 4th {}:'.format(order[0], order[1], order[2], order[3]))
     for i in range(0, len(fantasy_people)):
         print('{}.) {}'.format(i+1, fantasy_people[i]))
 
     print('')
 
-for team in wc_teams:
+for order in permutations:
     random.shuffle(fantasy_people)
-    print_order(team, fantasy_people)
+    print_order(order, fantasy_people)
